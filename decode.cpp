5724206264af704d9e2839c1d2bafae6f5cea9e5
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2024-12-10 13:08:03 -0500

    init commit

---
+++ decode.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+int pr[6];
+char box[2];
+int tar;
+bool is_ASC;
+bool flag;
+int main ()
+{
+//	char egg[3] = {256-57,256-61,'\0'};
+//	printf ("%s", egg);
+	int tmp = 0;
+	int sum = 0;
+	int cnt = 0;
+	//for unicode
+	int un_1 = 0;
+	int un_2 = 0;
+	
+	while ((tmp = getchar()) != EOF)
+	{
+//		if (tmp != '\n' || tmp != '.')
+//		{
+//			printf ("\nRestricted input is reqired!\nNo modifiction is suggested.\n");
+//			tmp=sum=cnt=un_1=un_2=0;
+//			memset (pr, 0, sizeof(pr));
+//			memset (box, '\0', sizeof(pr));
+//			putchar ('\n');
+//			
+//			continue;
+//		}
+		if (tmp == '\n')
+		{
+			tmp=sum=cnt=un_1=un_2=0;
+			memset (pr, 0, sizeof(pr));
+			memset (box, '\0', sizeof(pr));
+			putchar ('\n');
+			putchar ('\n');
+			tmp = getchar();
+			flag = true;
+		}
+		if (char(tmp) == '.')//ascll
+		{
+			is_ASC = true;
+			while (char(tmp = getchar()) == '.');
+			box[0] = tmp;
+			tmp = getchar();
+			box[1] = tmp;
+			
+			if 		(box == string("∫ﬂ")) pr[++tar] = 0; 
+			else if (box == string("‡≈")) pr[++tar] = 1; 
+			else if (box == string("∞°")) pr[++tar] = 2; 
+		
+//			printf ("%d", pr[tar]);
+		}
+		else//unicode
+		{
+			is_ASC = false;
+			
+//			tmp = -tmp;
+			box[0] = tmp;
+			tmp = getchar();
+//			tmp = -tmp;
+			box[1] = tmp;
+			
+			if 		(box == string("∫ﬂ")) pr[++tar] = 0; 
+			else if (box == string("‡≈")) pr[++tar] = 1; 
+			else if (box == string("∞°")) pr[++tar] = 2;
+			else
+			{
+				printf ("\n\nUndefined value: \'%c\' & \'%c\' \nRestricted input is reqired!\nNo modifiction is suggested.\n",box[0],box[1]);
+                system("pause");
+				return -1;
+			}
+//			printf ("%d", pr[tar]);
+		}
+		if (++cnt == 5)
+		{
+			cnt = sum = tar = 0;
+			sum += pr[1]*81;
+			sum += pr[2]*27;
+			sum += pr[3]*9;
+			sum += pr[4]*3;
+			sum += pr[5]*1;
+			if (is_ASC)
+			{
+//				if (flag) putchar ('\n');
+//				flag = false;
+				printf ("%c", char(sum));
+			}
+			else
+			{
+				if (un_1 == 0)//left uni
+				{
+					un_1 = sum + 128;
+				}
+				else //right uni
+				{
+//					if (flag) putchar ('\n');
+//					flag = false;
+					un_2 = sum + 128;
+					char bx[3] = {un_1, un_2, '\0'};
+//					printf ("%d %d\n", un_1, un_2);
+					
+					printf ("%s", bx);
+					un_1 = un_2 = 0;
+				}
+			}
+		}
+	}
+		
+}
---
+++ encode.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+//char map[3]{'∫ﬂ','‡≈','∞°'};
+bool cn[2000];//if unicode or ascll
+int pr[2000];
+int size_A;
+int size_U; 
+
+int main ()
+{
+	srand((unsigned)time(NULL));
+	int input[2000];
+	int output[2000];
+	char tmp;
+	int size = 0;
+	int cnt = 0;
+	int ch, mo;
+	int ans[6] = {0};//[5] 
+	
+	for(;;)
+	{
+		size_U = size_A = size = cnt = ch = mo = 0;
+		ans,pr,cn,input,output,
+		memset (ans, 0, sizeof(ans));
+		memset (pr, 0, sizeof(pr));
+		memset (cn, 0, sizeof(cn));
+		memset (input, 0, sizeof(input));
+		memset (output, 0, sizeof(output));
+		while ((tmp=getchar()) != '\n')
+		{
+			input[++size] = (int)(tmp);
+		}
+		putchar ('\n');
+		for (int i = 1; i <= size; i++)
+		{
+			ch = input[++cnt];
+			if (ch < 0)//if unicode or ascll
+			{
+				ch = ((unsigned char)ch) - 128;
+				size_U++;
+			}
+			else
+			{
+				cn[cnt] = 1;//mark ascll 
+				size_A++;
+			}
+//			printf ("%d = ", ch);
+			
+			int ttmp = 5;
+			memset (ans, 0, sizeof(ans));
+			while (ch != 0)
+			{
+				mo = ch % 3;
+				ch = (ch - mo) / 3;
+				ans[ttmp--] = mo;
+			}
+			
+			for (int j = 1; j <= 5; j++)
+			{
+//				printf ("%d ", ans[j]);
+				pr[(i-1)*5+j] = ans[j];
+			}
+//			if (cn[i])//log
+//			{
+//				printf ("ASC\n");
+//			}
+//			else printf ("UNI\n");
+//			putchar('\n');
+		}
+	//	for (int i = 1; i <= size * 5; i++)
+	//	{
+	//		printf ("%d ", pr[i]);
+	//	}
+	//	putchar('\n');
+		
+		int tar = 0;
+		for (int i = 1; i <= size*5; i++)
+		{
+			//printf ("%d ", pr[i]);
+			if (cn[int((i-1)/5)+1])
+			{
+				output[++tar] = 3;//print '.'
+//				printf ("%d ", output[tar]);
+			}
+	//		switch (pr[i])
+	//		{
+	//			case 0: output[++tar] = "∫ﬂ"; break;
+	//			case 1: output[++tar] = "‡≈"; break;
+	//			case 2: output[++tar] = "∞°"; break;
+	//		}
+			//output[++tar] = map[pr[i]];
+			output[++tar] = pr[i];//print '∫ﬂ‡≈∞°'
+//			printf ("%d ", output[tar]);
+		}
+//		putchar ('\n');
+//		putchar ('\n');
+		
+		cnt = 0;
+//		while (input[++cnt] != 0)//log
+//		{
+//			printf ("%d ", char(input[cnt]));
+//		}
+//		putchar ('\n');
+//		putchar ('\n');
+			
+		for (int i = 1; i <= (size_A*2+size_U)*5; i++)
+		{
+			//printf ("\n%d\n", (size_A*2+size_U)*5);
+			switch (output[i])
+			{
+				case 0: printf ("∫ﬂ"); break; 
+				case 1: printf ("‡≈"); break; 
+				case 2: printf ("∞°"); break;
+				case 3:
+				{
+					int dot = rand() % 3 + 2;
+					for (int j = 0; j < dot; j++)
+					{
+						printf (".");
+					}
+					break;
+				}
+			}
+		}
+		
+		
+		//printf ("%d", input);
+		putchar ('\n');
+		putchar ('\n');
+	}
+	system ("pause");
+	return 0;
+} 
