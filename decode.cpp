ff1747413baba6a9b595dfe0f0877b5762ab70a8
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2024-12-10 14:14:53 -0500

    style: format

--- decode.cpp
+++ decode.cpp
@@ -5,107 +5,113 @@ char box[2];
 int tar;
 bool is_ASC;
 bool flag;
-int main ()
+int main()
 {
-//	char egg[3] = {256-57,256-61,'\0'};
-//	printf ("%s", egg);
+	//	char egg[3] = {256-57,256-61,'\0'};
+	//	printf ("%s", egg);
 	int tmp = 0;
 	int sum = 0;
 	int cnt = 0;
-	//for unicode
+	// for unicode
 	int un_1 = 0;
 	int un_2 = 0;
-	
+
 	while ((tmp = getchar()) != EOF)
 	{
-//		if (tmp != '\n' || tmp != '.')
-//		{
-//			printf ("\nRestricted input is reqired!\nNo modifiction is suggested.\n");
-//			tmp=sum=cnt=un_1=un_2=0;
-//			memset (pr, 0, sizeof(pr));
-//			memset (box, '\0', sizeof(pr));
-//			putchar ('\n');
-//			
-//			continue;
-//		}
+		//		if (tmp != '\n' || tmp != '.')
+		//		{
+		//			printf ("\nRestricted input is reqired!\nNo modifiction is suggested.\n");
+		//			tmp=sum=cnt=un_1=un_2=0;
+		//			memset (pr, 0, sizeof(pr));
+		//			memset (box, '\0', sizeof(pr));
+		//			putchar ('\n');
+		//
+		//			continue;
+		//		}
 		if (tmp == '\n')
 		{
-			tmp=sum=cnt=un_1=un_2=0;
-			memset (pr, 0, sizeof(pr));
-			memset (box, '\0', sizeof(pr));
-			putchar ('\n');
-			putchar ('\n');
+			tmp = sum = cnt = un_1 = un_2 = 0;
+			memset(pr, 0, sizeof(pr));
+			memset(box, '\0', sizeof(pr));
+			putchar('\n');
+			putchar('\n');
 			tmp = getchar();
 			flag = true;
 		}
-		if (char(tmp) == '.')//ascll
+		if (char(tmp) == '.') // ascll
 		{
 			is_ASC = true;
-			while (char(tmp = getchar()) == '.');
+			while (char(tmp = getchar()) == '.')
+				;
 			box[0] = tmp;
 			tmp = getchar();
 			box[1] = tmp;
-			
-			if 		(box == string("哼")) pr[++tar] = 0; 
-			else if (box == string("嗯")) pr[++tar] = 1; 
-			else if (box == string("啊")) pr[++tar] = 2; 
-		
-//			printf ("%d", pr[tar]);
+
+			if (box == string("哼"))
+				pr[++tar] = 0;
+			else if (box == string("嗯"))
+				pr[++tar] = 1;
+			else if (box == string("啊"))
+				pr[++tar] = 2;
+
+			//			printf ("%d", pr[tar]);
 		}
-		else//unicode
+		else // unicode
 		{
 			is_ASC = false;
-			
-//			tmp = -tmp;
+
+			//			tmp = -tmp;
 			box[0] = tmp;
 			tmp = getchar();
-//			tmp = -tmp;
+			//			tmp = -tmp;
 			box[1] = tmp;
-			
-			if 		(box == string("哼")) pr[++tar] = 0; 
-			else if (box == string("嗯")) pr[++tar] = 1; 
-			else if (box == string("啊")) pr[++tar] = 2;
+
+			if (box == string("哼"))
+				pr[++tar] = 0;
+			else if (box == string("嗯"))
+				pr[++tar] = 1;
+			else if (box == string("啊"))
+				pr[++tar] = 2;
 			else
 			{
-				printf ("\n\nUndefined value: \'%c\' & \'%c\' \nRestricted input is reqired!\nNo modifiction is suggested.\n",box[0],box[1]);
-                system("pause");
+				printf("\n\nUndefined value: \'%c\' & \'%c\' \nRestricted input is reqired!\nNo modifiction is suggested.\n", box[0], box[1]);
+				system("pause");
 				return -1;
 			}
-//			printf ("%d", pr[tar]);
+			//			printf ("%d", pr[tar]);
 		}
 		if (++cnt == 5)
 		{
 			cnt = sum = tar = 0;
-			sum += pr[1]*81;
-			sum += pr[2]*27;
-			sum += pr[3]*9;
-			sum += pr[4]*3;
-			sum += pr[5]*1;
+			sum += pr[1] * 81;
+			sum += pr[2] * 27;
+			sum += pr[3] * 9;
+			sum += pr[4] * 3;
+			sum += pr[5] * 1;
 			if (is_ASC)
 			{
-//				if (flag) putchar ('\n');
-//				flag = false;
-				printf ("%c", char(sum));
+				//				if (flag) putchar ('\n');
+				//				flag = false;
+				printf("%c", char(sum));
 			}
 			else
 			{
-				if (un_1 == 0)//left uni
+				if (un_1 == 0) // left uni
 				{
 					un_1 = sum + 128;
 				}
-				else //right uni
+				else // right uni
 				{
-//					if (flag) putchar ('\n');
-//					flag = false;
+					//					if (flag) putchar ('\n');
+					//					flag = false;
 					un_2 = sum + 128;
 					char bx[3] = {un_1, un_2, '\0'};
-//					printf ("%d %d\n", un_1, un_2);
-					
-					printf ("%s", bx);
+					//					printf ("%d %d\n", un_1, un_2);
+
+					printf("%s", bx);
 					un_1 = un_2 = 0;
 				}
 			}
 		}
 	}
-		
 }
--- encode.cpp
+++ encode.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-//char map[3]{'哼','嗯','啊'};
-bool cn[2000];//if unicode or ascll
+// char map[3]{'哼','嗯','啊'};
+bool cn[2000]; // if unicode or ascll
 int pr[2000];
 int size_A;
-int size_U; 
+int size_U;
 
-int main ()
+int main()
 {
 	srand((unsigned)time(NULL));
 	int input[2000];
@@ -15,119 +15,124 @@ int main ()
 	int size = 0;
 	int cnt = 0;
 	int ch, mo;
-	int ans[6] = {0};//[5] 
-	
-	for(;;)
+	int ans[6] = {0}; //[5]
+
+	for (;;)
 	{
 		size_U = size_A = size = cnt = ch = mo = 0;
-		ans,pr,cn,input,output,
-		memset (ans, 0, sizeof(ans));
-		memset (pr, 0, sizeof(pr));
-		memset (cn, 0, sizeof(cn));
-		memset (input, 0, sizeof(input));
-		memset (output, 0, sizeof(output));
-		while ((tmp=getchar()) != '\n')
+		ans, pr, cn, input, output,
+			memset(ans, 0, sizeof(ans));
+		memset(pr, 0, sizeof(pr));
+		memset(cn, 0, sizeof(cn));
+		memset(input, 0, sizeof(input));
+		memset(output, 0, sizeof(output));
+		while ((tmp = getchar()) != '\n')
 		{
 			input[++size] = (int)(tmp);
 		}
-		putchar ('\n');
+		putchar('\n');
 		for (int i = 1; i <= size; i++)
 		{
 			ch = input[++cnt];
-			if (ch < 0)//if unicode or ascll
+			if (ch < 0) // if unicode or ascll
 			{
 				ch = ((unsigned char)ch) - 128;
 				size_U++;
 			}
 			else
 			{
-				cn[cnt] = 1;//mark ascll 
+				cn[cnt] = 1; // mark ascll
 				size_A++;
 			}
-//			printf ("%d = ", ch);
-			
+			//			printf ("%d = ", ch);
+
 			int ttmp = 5;
-			memset (ans, 0, sizeof(ans));
+			memset(ans, 0, sizeof(ans));
 			while (ch != 0)
 			{
 				mo = ch % 3;
 				ch = (ch - mo) / 3;
 				ans[ttmp--] = mo;
 			}
-			
+
 			for (int j = 1; j <= 5; j++)
 			{
-//				printf ("%d ", ans[j]);
-				pr[(i-1)*5+j] = ans[j];
+				//				printf ("%d ", ans[j]);
+				pr[(i - 1) * 5 + j] = ans[j];
 			}
-//			if (cn[i])//log
-//			{
-//				printf ("ASC\n");
-//			}
-//			else printf ("UNI\n");
-//			putchar('\n');
+			//			if (cn[i])//log
+			//			{
+			//				printf ("ASC\n");
+			//			}
+			//			else printf ("UNI\n");
+			//			putchar('\n');
 		}
-	//	for (int i = 1; i <= size * 5; i++)
-	//	{
-	//		printf ("%d ", pr[i]);
-	//	}
-	//	putchar('\n');
-		
+		//	for (int i = 1; i <= size * 5; i++)
+		//	{
+		//		printf ("%d ", pr[i]);
+		//	}
+		//	putchar('\n');
+
 		int tar = 0;
-		for (int i = 1; i <= size*5; i++)
+		for (int i = 1; i <= size * 5; i++)
 		{
-			//printf ("%d ", pr[i]);
-			if (cn[int((i-1)/5)+1])
+			// printf ("%d ", pr[i]);
+			if (cn[int((i - 1) / 5) + 1])
 			{
-				output[++tar] = 3;//print '.'
-//				printf ("%d ", output[tar]);
+				output[++tar] = 3; // print '.'
+								   //				printf ("%d ", output[tar]);
 			}
-	//		switch (pr[i])
-	//		{
-	//			case 0: output[++tar] = "哼"; break;
-	//			case 1: output[++tar] = "嗯"; break;
-	//			case 2: output[++tar] = "啊"; break;
-	//		}
-			//output[++tar] = map[pr[i]];
-			output[++tar] = pr[i];//print '哼嗯啊'
-//			printf ("%d ", output[tar]);
+			//		switch (pr[i])
+			//		{
+			//			case 0: output[++tar] = "哼"; break;
+			//			case 1: output[++tar] = "嗯"; break;
+			//			case 2: output[++tar] = "啊"; break;
+			//		}
+			// output[++tar] = map[pr[i]];
+			output[++tar] = pr[i]; // print '哼嗯啊'
+								   //			printf ("%d ", output[tar]);
 		}
-//		putchar ('\n');
-//		putchar ('\n');
-		
+		//		putchar ('\n');
+		//		putchar ('\n');
+
 		cnt = 0;
-//		while (input[++cnt] != 0)//log
-//		{
-//			printf ("%d ", char(input[cnt]));
-//		}
-//		putchar ('\n');
-//		putchar ('\n');
-			
-		for (int i = 1; i <= (size_A*2+size_U)*5; i++)
+		//		while (input[++cnt] != 0)//log
+		//		{
+		//			printf ("%d ", char(input[cnt]));
+		//		}
+		//		putchar ('\n');
+		//		putchar ('\n');
+
+		for (int i = 1; i <= (size_A * 2 + size_U) * 5; i++)
 		{
-			//printf ("\n%d\n", (size_A*2+size_U)*5);
+			// printf ("\n%d\n", (size_A*2+size_U)*5);
 			switch (output[i])
 			{
-				case 0: printf ("哼"); break; 
-				case 1: printf ("嗯"); break; 
-				case 2: printf ("啊"); break;
-				case 3:
+			case 0:
+				printf("哼");
+				break;
+			case 1:
+				printf("嗯");
+				break;
+			case 2:
+				printf("啊");
+				break;
+			case 3:
+			{
+				int dot = rand() % 3 + 2;
+				for (int j = 0; j < dot; j++)
 				{
-					int dot = rand() % 3 + 2;
-					for (int j = 0; j < dot; j++)
-					{
-						printf (".");
-					}
-					break;
+					printf(".");
 				}
+				break;
+			}
 			}
 		}
-		
-		
-		//printf ("%d", input);
-		putchar ('\n');
-		putchar ('\n');
+
+		// printf ("%d", input);
+		putchar('\n');
+		putchar('\n');
 	}
-	system ("pause");
+	system("pause");
 	return 0;
-} 
+}
